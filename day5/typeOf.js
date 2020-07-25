ffb24e51d2140df7bfdf3fc4878df5ce02a3aa57
NightSlayer <praneethkumarpk34@gmail.com>
2020-07-26 00:30:34 +0530

    update

---
+++ day5/typeOf.js
@@ -0,0 +1,17 @@
+console.log(typeof 42);
+// expected output: "number"
+
+console.log(typeof "blubber");
+// expected output: "string"
+
+console.log(typeof true);
+// expected output: "boolean"
+
+console.log(typeof undeclaredVariable);
+// expected output: "undefined"
+
+console.log(typeof null);
+// expected output: "object"
+
+console.log(typeof func);
+// expected output: 'function'
