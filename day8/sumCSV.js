e5fc287a99044baa099500da43a911e75cafe91a
NightSlayer <praneethkumarpk34@gmail.com>
2020-07-30 11:29:59 +0530

    changed code using inline function

--- day8/sumCSV.js
+++ day8/sumCSV.js
@@ -1,13 +1,13 @@
 /** Calculate the sum of numbers received in a comma delimited string */
 
-console.log(sumCSV("1.5, 2.3, 3.1, 4, 5.5, 6, 7, 8, 9, 10.9"));
+//console.log(sumCSV("1.5, 2.3, 3.1, 4, 5.5, 6, 7, 8, 9, 10.9"));
 
-function sumCSV(s) {
+(function (s) {
   let arr = [];
   arr = s.split(",").map((val) => +val);
 
   var sum = arr.reduce(function (a, b) {
     return a + b;
   }, 0);
-  return sum;
-}
+  console.log(sum);
+})("1.5, 2.3, 3.1, 4, 5.5, 6, 7, 8, 9, 10.9");
